27566fa5a563cba6bac4b893f92a070c45ad29cb
sachinv26 <vermas7008@gmail.com>
2022-12-27 12:30:44 +0530

    fibonacci programs added

---
+++ fibonacci_memoization.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int memo[1000];
+int fib(int n)
+{
+    if (memo[n] == -1)
+    {
+        int res;
+        if (n == 1 || n == 0)
+            return n;
+        else
+        {
+            res = fib(n - 1) + fib(n - 2);
+        }
+        memo[n] = res;
+    }
+    return memo[n];
+}
+int main()
+{
+    int n = 4;
+    memset(memo, -1, sizeof(memo));
+    cout << fib(n);
+    return 0;
+}
---
+++ fibonacci_spaceoptimized.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fib(int n)
+{
+    int a = 0, b = 1, c, i;
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fib(n);
+    return 0;
+}
